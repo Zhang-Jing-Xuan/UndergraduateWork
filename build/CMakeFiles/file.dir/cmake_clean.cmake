8711f2558c5e7b73b460dcdc86828c9f0205f2ae
Zhang-Jing-Xuan <zjx617@yeah.net>
2021-06-24 09:22:30 +0800

    add build

---
+++ build/CMakeFiles/file.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/file.dir/FileSys.cpp.obj"
+  "CMakeFiles/file.dir/FileSys.cpp.obj.d"
+  "CMakeFiles/file.dir/main.cpp.obj"
+  "CMakeFiles/file.dir/main.cpp.obj.d"
+  "file.exe"
+  "file.exe.manifest"
+  "file.pdb"
+  "libfile.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/file.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Code/FILESYSTEM/FileSystem
+# Build directory: C:/Code/FILESYSTEM/FileSystem/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
