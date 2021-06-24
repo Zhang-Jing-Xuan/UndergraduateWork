af6c46e7d5fe80fb0622ddc4156c3d58b8f4476e
Zhang-Jing-Xuan <70561649+Zhang-Jing-Xuan@users.noreply.github.com>
2021-06-24 09:41:55 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Code/FILESYSTEM/FileSystem
-# Build directory: C:/Code/FILESYSTEM/FileSystem/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
