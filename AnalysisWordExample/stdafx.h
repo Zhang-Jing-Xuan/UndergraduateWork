7b9064e2513c6b136593de518b69b6d10e40f8b1
Zhang-Jing-Xuan <zjx617@yeah.net>
2021-07-14 17:57:11 +0800

    词法分析程序样例

---
+++ AnalysisWordExample/stdafx.h
@@ -0,0 +1,12 @@
+#ifndef zjx
+#define zjx
+
+#define MAX_LENGTH_OF_A_WORD 20     //一个单词的最多字符个数
+#define NUMBER_OF_RESERVED_WORDS 19 //保留字个数
+#define MAX_NUMBER_OF_WORDS 1000    //可识别的最多单词个数
+#define OK 1                        //返回一个真值
+#define ERROR 0                     //返回一个假值
+#define SPACE 32                    //SPACE空格的ASCII码值
+#define RETURN 10                   //RETURN换行的ASCII码值
+#define TABLE 9                     //TABLE的ASCII码值
+#endif
