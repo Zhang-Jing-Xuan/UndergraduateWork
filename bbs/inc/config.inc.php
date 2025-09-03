3d6e556ae0714c07812093c9c5a5f3130a49b16f
Zhang-Jing-Xuan <zjx617@yeah.net>
2025-09-03 17:29:46 +0800

    Move bbs files to subdirectory

---
+++ bbs/inc/config.inc.php
@@ -0,0 +1,15 @@
+<?php
+    date_default_timezone_set('Asia/Shanghai');
+    session_start();
+    define ('DB_HOST','127.0.0.1');
+    define ('DB_USER','root');
+    define ('DB_PASSWORD','271828');
+    define ('DB_DATABASE','bbs');
+    define ('DB_PORT',3306);
+    define('SA_PATH',dirname(dirname(__FILE__)));
+    define('SUB_URL',str_replace($_SERVER['DOCUMENT_ROOT'],'',str_replace('\\','/',SA_PATH)).'/');
+    // echo str_replace($_SERVER['DOCUMENT_ROOT'],'',str_replace('\\','/',SA_PATH)).'/';
+    // var_dump($_SERVER['DOCUMENT_ROOT']);
+    error_reporting(0); ##
+?>
+
